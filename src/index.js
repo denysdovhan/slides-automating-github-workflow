2976a37b745edc4d2d71b1c03c7e5adf84b528ec
Denys Dovhan <denysdovhan@gmail.com>
2018-05-22 22:37:54 +0300

    Updated at Tue, 22 May 2018, 22:37:43, EEST

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+const rootEl = document.getElementById('root');
+
+const render = Component => {
+  ReactDOM.render(<Component />, rootEl);
+};
+
+render(App);
+
+if (module.hot) {
+  module.hot.accept('./App', () => render(App));
+}
+
+registerServiceWorker();
